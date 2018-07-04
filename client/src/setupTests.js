b8b1540ba2d00852c20579223d1f3cf9c6dd58ca
philm <philmayfield@gmail.com>
2018-07-03 23:31:51 -0700

    fix test setup with svgs

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -3,3 +3,7 @@ import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 Enzyme.configure({ adapter: new Adapter() });
+
+if (!global.SVGSVGElement && global.HTMLUnknownElement) {
+  global.SVGSVGElement = global.HTMLUnknownElement;
+}
